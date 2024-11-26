587cbffd8be8625eb48b61f7e336fce1cce3fdb8
bvatai-br <balazs.vatai@bitrise.io>
2024-11-26 16:52:31 +0100

    Cleanup

--- src/main/java/com/google/devtools/build/lib/remote/util/DigestUtil.java
+++ src/main/java/com/google/devtools/build/lib/remote/util/DigestUtil.java
@@ -92,7 +92,6 @@ public class DigestUtil {
   }
 
   public Digest compute(VirtualActionInput input) throws IOException {
-    // TOGGLE: 18
     ByteArrayOutputStream buffer = new ByteArrayOutputStream();
     input.writeTo(buffer);
     return compute(buffer.toByteArray());
